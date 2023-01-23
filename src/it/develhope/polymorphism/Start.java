49ab16b7917a558f5bf4c52861ec2b2ba3f955b6
Alessio Limina <alessio.limina@gmail.com>
2023-01-23 21:13:31 +0100

    Exercise fixing

--- src/it/develhope/polymorphism/Start.java
+++ src/it/develhope/polymorphism/Start.java
@@ -6,23 +6,29 @@ public class Start {
     public static void main(String[] args) {
 
     System.out.println("------------Starting-----------------");
+
     System.out.println("Type an animal: ");
 
     Scanner scanner = new Scanner(System.in);
 
     Animal animal = new Animal(scanner.nextLine());
+    System.out.println("You typed '" + animal.animalName + "'. What's his sound?");
     animal.animalSound();
 
     System.out.println("------------------------------------");
 
     System.out.println("Type an animal: ");
+
     Lion lion = new Lion(scanner.nextLine());
+    System.out.println("You typed '" + lion.animalName  + "'. What's his sound?");
     lion.animalSound();
 
     System.out.println("------------------------------------");
 
     System.out.println("Type an animal: ");
+
     Cow cow = new Cow(scanner.nextLine());
+    System.out.println("You typed '" + cow.animalName + "'. What's her sound?");
     cow.animalSound();
 
     System.out.println("------------------------------------");
